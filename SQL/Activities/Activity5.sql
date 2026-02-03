274acfd0f56d77f63316b1bba5a0485ba3bf3b25
BalaPriyaDarshiniA <bala.priya.darshini.a@ibm.com>
2026-02-03 18:41:55 +0530

    Add files via upload

---
+++ SQL/Activities/Activity5.sql
@@ -0,0 +1,10 @@
+-- Update the grade score of salesmen from Rome to 200.
+UPDATE salesman SET grade=200 WHERE salesman_city='Rome';
+
+-- Update the grade score of James Hoog to 300.
+UPDATE salesman SET grade=300 WHERE salesman_name='James Hoog';
+
+
+UPDATE salesman SET salesman_name='Pierre' WHERE salesman_name='McLyon';
+
+SELECT * FROM salesman;
